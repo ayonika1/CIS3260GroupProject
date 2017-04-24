a0b048b4b445131313ebd6a1358eab6f0ce385d2
ayonika1 <ayonika1@student.gsu.edu>
2017-04-24 13:33:01 -0400

    Update toaster.java

--- CIS3260_Group_Project/src/toaster.java
+++ CIS3260_Group_Project/src/toaster.java
@@ -1,8 +1,22 @@
 
 public class toaster {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) {
+		int lightOption = Integer.parseInt(JOptionPane.showInputDialog(null, "Would you like toast?\n" 
+						+ "1.Turn on toaster\n" + "2.Do not turn on toaster\n" + "3.Return to main menu\n"));
+		
+		if (makeToast == 1) {
+		//	make toast;
+		}
+		
+		//we need to make an option and research how to return to the main menu.
+		
+	}
+	
+	public static boolean makeToast;
 	{
+		boolean toast = true;
+		System.out.println("The toaster is heating up");
 
 	}
 }
