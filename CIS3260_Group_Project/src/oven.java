f8ee57ab5eabdd7bc37c96fd0840ab0b6e089747
ayonika1 <ayonika1@student.gsu.edu>
2017-04-24 13:46:55 -0400

    Update oven.java

--- CIS3260_Group_Project/src/oven.java
+++ CIS3260_Group_Project/src/oven.java
@@ -1,9 +1,23 @@
 
 public class oven {
 
-	public static void main(String[] args) 
-	{
-
+	public static void main(String[] args) {
+		int ovenOption = Integer.parseInt(JOptionPane.showInputDialog(null, "What would you like to do?\n" 
+						+ "1.Turn oven on\n" + "2.Turn oven off\n" + "3.Return to main menu\n"));
+		
+		if (ovenOption == 1) {
+			ovenOn=true
+		}
+		else {
+			ovenOn=false
+		}
+		
+	}
+	
+	public static boolean ovenOn; {
+		if (boolean ovenOn = true) {
+			System.out.println("The oven is on");
+		}
 	}
 
 }
