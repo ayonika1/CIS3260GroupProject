b81986347de4877a2aaebccd1f378e9ad71f5509
ayonika1 <ayonika1@student.gsu.edu>
2017-04-24 14:02:30 -0400

    Update doors.java

--- CIS3260_Group_Project/src/doors.java
+++ CIS3260_Group_Project/src/doors.java
@@ -1,8 +1,30 @@
 
 public class doors {
 
-	public static void main(String[] args) 
-	{
-
+	public static void main(String[] args)  {
+		
+		System.out.println("Are the doors open? " + doorStatus) ;
+	
+		int doorOption = Integer.parseInt(JOptionPane.showInputDialog(null, "What do you want to do?\n" 
+					+ "1.Open Door\n" + "2.Close door\n" + "3.Return to main menu\n"));
+	
+		if (doorOption ==1 ){
+			doorAction = true;
+		}
+	
 	}
+	
+	
+	
+	public static boolean doorAction; {
+		boolean door = true;
+		System.out.println("The door is now open");
+	}
+
+	public static boolean doorStatus {
+		if (doorAction == true) {
+			return true;
+		}
+		else
+			return false;
 }
